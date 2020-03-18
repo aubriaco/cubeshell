7d97a969683f970ce89f63b4a0ffdf9621c1d194
Alessandro Ubriaco <alessandro.ubriaco@gmail.com>
2020-03-18 16:17:52 -0400

    Updates

--- libshellcubedb/src/CDatabase.cpp
+++ libshellcubedb/src/CDatabase.cpp
@@ -1,10 +1,14 @@
-#include <CDatabase.h>
+#include "CDatabase.h"
+
+static size_t DBC = 0;
 
 CDatabase::CDatabase()
 {
   if(DBC == 0)
-    mongodb_init();
+    mongoc_init();
   DBC++;
+  Client = 0;
+  DB = 0;
 }
 
 CDatabase::~CDatabase()
@@ -19,10 +23,30 @@ void CDatabase::setConnectionString(const std::string& cs)
   ConnectionString = cs;
 }
 
+void CDatabase::setDatabaseName(const std::string& name)
+{
+  DBName = name;
+}
+
 void CDatabase::open()
 {
+  mongoc_uri_t *uri = mongoc_uri_new(ConnectionString.c_str());
+  Client = mongoc_client_new_from_uri(uri);
+  mongoc_client_set_appname(Client, "ShellCubeDB");
+  DB = mongoc_client_get_database(Client, DBName.c_str());
+  mongoc_uri_destroy(uri);
 }
 
 void CDatabase::close()
 {
+  if(DB)
+  {
+    mongoc_database_destroy(DB);
+    DB = 0;
+  }
+  if(Client)
+  {
+    mongoc_client_destroy(Client);
+    Client = 0;
+  }
 }
--- libshellcubedb/src/CDatabase.h
+++ libshellcubedb/src/CDatabase.h
@@ -1,16 +1,19 @@
 #ifndef __C_DATABASE_INCLUDED__
 #define __C_DATABASE_INCLUDED__
-#include <IDatabase.h>
+#include <shellcubedb/IDatabase.h>
+#include <mongoc/mongoc.h>
 
 class CDatabase
 {
 private:
-  std::string ConnectionString;
-  static size_t DBC = 0;
+  std::string ConnectionString, DBName;
+  mongoc_client_t *Client;
+  mongoc_database_t *DB;
 public:
   CDatabase();
   ~CDatabase();
   void setConnectionString(const std::string& cs);
+  void setDatabaseName(const std::string& name);
   void open();
   void close();
 };
