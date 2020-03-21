3ccf62073571c7c40f9ff9ba0ade0a7b4e74babd
Alessandro Ubriaco <alessandro.ubriaco@gmail.com>
2020-03-21 18:47:12 -0400

    CLI sha pwd

--- node/src/cli_server.cpp
+++ node/src/cli_server.cpp
@@ -2,8 +2,26 @@
 #include <solunet.h>
 #include <cubeshelldb.h>
 #include "main.h"
+#include <openssl/sha.h>
 
 using namespace cubeshell;
+
+std::string sha256_string(const std::string &s)
+{
+  char outputBuffer[65];
+  unsigned char hash[SHA256_DIGEST_LENGTH];
+  SHA256_CTX sha256;
+  SHA256_Init(&sha256);
+  SHA256_Update(&sha256, s.c_str(), s.size());
+  SHA256_Final(hash, &sha256);
+  int i = 0;
+  for(i = 0; i < SHA256_DIGEST_LENGTH; i++)
+  {
+    sprintf(outputBuffer + (i * 2), "%02x", hash[i]);
+  }
+  outputBuffer[64] = 0;
+  return outputBuffer;
+}
 
 int cli_cmd(IDatabase *db, std::string s)
 {
@@ -74,10 +92,17 @@ void *cli_server(void* param)
 
         if(u.size() > 0)
         {
-          int r = 0;
-          socket->writeBuffer(&r, 4);
-          loggedIn = true;
-          fprintf(stdout, "Logged in.\n");
+          fprintf(stdout, "User found.\n");
+          if(u[0]["password"] == sha256_string(password))
+          {
+            fprintf(stdout, "Password OK.\n");
+            int r = 0;
+            socket->writeBuffer(&r, 4);
+            loggedIn = true;
+            fprintf(stdout, "Logged in.\n");
+          }
+          else
+            fprintf(stdout, "Invalid Password.\n");
         }
 
         if(!loggedIn)
